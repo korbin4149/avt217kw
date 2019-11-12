dacab87c98e80a6ece0f9f1a07c63fc2dc3cc1c5
korbin4149 <54602970+korbin4149@users.noreply.github.com>
2019-11-12 18:59:50 -0500

    bleh

--- e17/js/main.js
+++ e17/js/main.js
@@ -1,6 +1,22 @@
-$(document ).ready(function() {
-  console.log('This site thou art has created has loaded good sir');
 
+$(document ).ready(function()
+{
+  console.log("This site thou art has created has loaded good sir");
+
+  $(".seven").click(function()
+  {
+    $(".number1").toggleClass("align-left")
+  });
+
+  $(".fifty").click(function()
+  {
+    $(".number2").toggleClass("spin")
+  });
+
+  $(".twenty").click(function()
+  {
+    $(".number3").toggleClass("spinup")
+  });
 
 
 
