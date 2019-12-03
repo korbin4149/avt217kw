e7cd728b1a72ba21e5ad00cc07e665137ae2b9a1
korbin4149 <54602970+korbin4149@users.noreply.github.com>
2019-12-03 18:27:26 -0500

    done

--- p5/js/main.js
+++ p5/js/main.js
@@ -3,14 +3,18 @@ $(document).ready(function()
   console.log("This site, thou art has created has loaded good sir");
 
 
-  $(".first").click(function()
+  $(".play").click(function()
   {
     $(".second").toggleClass("show")
   });
 
-  $(".first").click(function()
+  $(".play").click(function()
   {
     $(".first").remove()
+  });
+  $(".play").click(function()
+  {
+    $(".play").remove()
   });
 
 
